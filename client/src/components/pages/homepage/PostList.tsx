01397a558e0a90e8eeafe742bf8a2083d05c1941
MarkiMark01 <markishgroup@gmail.com>
2025-02-28 15:41:35 +0100

    Added styles to PostList

--- client/src/components/pages/homepage/PostList.tsx
+++ client/src/components/pages/homepage/PostList.tsx
@@ -1,25 +1,30 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { BeatLoader } from "react-spinners";
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 import PostItem from "./PostItem";
 import { PostListProps } from "../../types/Posts";
 import styles from "../../styles/homepage/postlist.module.scss";
-import { BeatLoader } from "react-spinners";
 
 const PostList: React.FC<PostListProps> = ({ posts, handleDeletePost }) => {
+  const [timeElapsed, setTimeElapsed] = useState(0);
+  const [serverReady, setServerReady] = useState(false);
+
   const theme = useSelector((state: RootState) => state.theme.mode);
-  
-  const [timeLeft, setTimeLeft] = useState(50);
 
   useEffect(() => {
-    if (posts.length === 0 && timeLeft > 0) {
-      const timer = setInterval(() => {
-        setTimeLeft((prevTime) => prevTime - 1);
-      }, 1000); 
+    const interval = setInterval(() => {
+      setTimeElapsed((prevTime) => prevTime + 1);
+    }, 1000);
 
-      return () => clearInterval(timer);
-    }
-  }, [posts, timeLeft]);
+
+    setTimeout(() => {
+      setServerReady(true);
+      clearInterval(interval);
+    }, 50000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <ul className={`${styles.postList} ${theme === "dark" ? styles.dark : ""}`}>
@@ -29,9 +34,17 @@ const PostList: React.FC<PostListProps> = ({ posts, handleDeletePost }) => {
         ))
       ) : (
         <p className={`${styles.noPosts} ${theme === "dark" ? styles.darkText : ""}`}>
-          <BeatLoader />
-          {timeLeft > 0 ? (
-            <span className={styles.timeLeft}>{`Time left: ${timeLeft}s`}</span>
+          {!serverReady ? (
+            <>
+              <BeatLoader color="#ff6347" />
+              <span className={styles.timeLeft}>
+                {`Time left: ${timeElapsed}s`}
+              </span>
+              <span className={styles.serverMessage}>
+                {" "}
+                Please note: The server is hosted on render.com, so it may take a little longer than expected. Thank you for your patience!
+              </span>
+            </>
           ) : (
             <span className={styles.serverReady}>Server is ready!</span>
           )}
@@ -42,6 +55,7 @@ const PostList: React.FC<PostListProps> = ({ posts, handleDeletePost }) => {
 };
 
 export default PostList;
+
 
 
 
